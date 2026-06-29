4ca9b4caa4be2884dd04d097f4115df9560ee23b
yoon <yoonicorn0207@gmail.com>
2026-06-29 11:31:13 +0900

    "feat : BlackRabbit 메인페이지 일별캔들 차트 구현"

--- src/main/java/com/blackrabbit/stock/StockController.java
+++ src/main/java/com/blackrabbit/stock/StockController.java
@@ -3,11 +3,12 @@ package com.blackrabbit.stock;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class StockController {
@@ -28,5 +29,32 @@ public class StockController {
     public List<StockDTO> getPresent_stockList() {
         return stockService.getPresent_StockList();
     }//BlackRabbit 메인페이지 - WatchList 데이터 호출 [HC_stock_daily1 테이블] (2026_0626에 추가)
+
+
+    /* 2. BlackRabbit 메인페이지 - 일별차트 (2026_0629) */
+    @RequestMapping("/api/chartData")
+    @ResponseBody
+    public List<Map<String, Object>> getChartData(@RequestParam("code") String code) {
+        // 1. Service를 통해 DB에서 데이터 조회
+        List<StockDailyDTO> dailyList = stockService.getDailyStockChartData(code);
+
+
+        // 2. ApexCharts가 인식할 수 있는 JSON 구조로 변환
+        return dailyList.stream().map((StockDailyDTO sddto) -> {
+            Map<String, Object> map = new HashMap<>();
+
+            // 이제 여기서 sddto를 사용하면 됩니다.
+            map.put("x", sddto.getStck_bsop_date());
+            map.put("y", new double[]{
+                    sddto.getStck_oprc(), // 시가
+                    sddto.getStck_hgpr(), // 고가
+                    sddto.getStck_lwpr(), // 저가
+                    sddto.getStck_clpr()  // 종가
+            });
+            return map;
+        }).collect(Collectors.toList());
+
+    }//BlackRabbit 메인페이지 - 일별차트
+
 
 }// StockController
--- src/main/java/com/blackrabbit/stock/StockService.java
+++ src/main/java/com/blackrabbit/stock/StockService.java
@@ -1,11 +1,17 @@
 package com.blackrabbit.stock;
 
 import java.util.List;
+import java.util.Map;
 
 public interface StockService {
 
     /* 1. BlackRabbit 메인페이지 - WatchList (2026_0626에 추가) */
     List<StockDTO> getPresent_StockList();
+
+
+    /* 2. BlackRabbit 메인페이지 - 일별차트 (2026_0629) */
+    List<StockDailyDTO> getDailyStockChartData(String code);
+
 
 
 
