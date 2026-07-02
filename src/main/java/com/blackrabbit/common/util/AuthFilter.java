408b87354ce60588b71e48905b058cb965aabb9f
js37hwang <js37hwang@gmail.com>
2026-07-02 14:58:03 +0900

    :sparkles: authFilter 화이트리스트 방식으로 변경

--- src/main/java/com/blackrabbit/common/util/AuthFilter.java
+++ src/main/java/com/blackrabbit/common/util/AuthFilter.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthFilter implements Filter {
-  // 보호가 필요한 라우터 리스트
-  private static final String[] PROTECTED_PATHS = {
-      "/stockMain",
+  // 보호가 필요하지 않은 라우터 리스트- 화이트리스트 방식으로 진행
+  private static final String[] PUBLIC_PATHS = {
+      "/login",
   };
 
-  private boolean isProtected(String uri) {
-    for (String path : PROTECTED_PATHS) {
+  private boolean isPublic(String uri) {
+    for (String path : PUBLIC_PATHS) {
       if (uri.startsWith(path)) return true;
     }
     return false;
@@ -28,22 +28,25 @@ public class AuthFilter implements Filter {
     String requestURI = httpRequest.getRequestURI();
     HttpSession session = httpRequest.getSession(false);
 
-    if (isProtected(requestURI)) {
+    // 공개 페이지 접근 시 return
+    if (isPublic(requestURI)) {
+      chain.doFilter(request, response);
+      return;
+    }
 
-      // 로그인 여부 확인
-      if (session == null || session.getAttribute("userId") == null) {
-        httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
-        return;
-      }
+    // 로그인 여부 확인
+    if (session == null || session.getAttribute("userId") == null) {
+      httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
+      return;
+    }
 
-      // 추후 관리자 페이지 생성 시 접근자의 권한 확인
+    // 추후 관리자 페이지 생성 시 접근자의 권한 확인
 //      if (!"ADMIN".equals(session.getAttribute("userRole"))) {
 //        httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp?error=unauthorized");
 //        return;
 //      }
-    }
 
     // 조건에 해당하지 않거나, 권한이 확인된 경우 요청을 통과시킴
-    chain.doFilter(request, response);
+      chain.doFilter(request,response);
   }
 }
