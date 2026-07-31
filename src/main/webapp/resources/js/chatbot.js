cfc3be83d9256b426811c994ef60e86dfcf5f51f
yoon <yoonicorn0207@gmail.com>
2026-07-31 09:25:12 +0900

    "feat : chat봇 수정"

--- src/main/webapp/resources/js/chatbot.js
+++ src/main/webapp/resources/js/chatbot.js
@@ -275,7 +275,10 @@ document.addEventListener('DOMContentLoaded', () => {
   function getSessionId() {
     let id = localStorage.getItem('chatSessionId');
     if (!id) {
-      id = crypto.randomUUID();
+      id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
       localStorage.setItem('chatSessionId', id);
     }
     return id;
